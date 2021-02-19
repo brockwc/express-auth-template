03233753fec688fcca0315aafa03278646b22a07
brockwc <brock.whitbread@gmail.com>
2021-02-19 12:31:55 -0800

    configures express session middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +11,14 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+app.use(session({
+  // a string used to generate a unique 
+  // session ID cookie to share with the browser
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true 
+}))
 
 app.get('/', (req, res) => {
   res.render('index');
